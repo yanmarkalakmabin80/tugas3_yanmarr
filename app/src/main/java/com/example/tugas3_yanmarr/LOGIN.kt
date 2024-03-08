89615199e15947a69eb38c74e352585c15ebb6a5
yanmar kalakmabin <yanmarkalakmabin80@gmail.com>
2024-03-08 18:09:30 +0900

    Initial commit

---
+++ app/src/main/java/com/example/tugas3_yanmarr/LOGIN.kt
@@ -0,0 +1,11 @@
+package com.example.tugas3_yanmarr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LOGIN : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/example/tugas3_yanmarr/lupapassword.kt
@@ -0,0 +1,11 @@
+package com.example.tugas3_yanmarr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class lupapassword : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lupapassword)
+    }
+}
